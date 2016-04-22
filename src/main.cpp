2ee6b5b4a2321029c46e92dedfb226437c6fe1c7
Mike <mike.deberg@gmail.com>
2016-04-22 12:57:19 -0700

    Now working

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,13 @@
 
 using namespace std;
 
+// stores one header and one sequence each can be up to 80 cahracters long
 struct record{
     string header;
     string sequence;
 };
 
-
+// retrieves a single record (header, sequence) from FASTA file
 record get_record(ifstream& ifs){
     string this_header;
     string this_seq;
@@ -31,12 +32,18 @@ int main(int argc, char **argv) {
     CompactPointer<Trie> prefix = new Trie();
     cout << "Pointer size: " << sizeof(&prefix) << "\n";;
 
+    // creating input stream for handling FASTA file
     ifstream ifs;
+    // opening input FASTA file
     ifs.open ("test_in.fasta", ifstream::in);
 
+    // creating record struct and char for end of file probe
     record this_record;
     char x;
-    while( ifs.get(x)) {        // This is not working
+
+    // while there is another character in the FASTA file
+    while( ifs.get(x)) {
+        // retrieve one record from FASTA file
         this_record = get_record(ifs);
         prefix->add(this_record.sequence);
         cout << "Prefix Length: " << prefix->get_size() << "\n";
