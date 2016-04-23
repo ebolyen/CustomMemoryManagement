7a8593fb9393bd42ddf64ab23ec00de98b5b00db
Mike <mike.deberg@gmail.com>
2016-04-23 10:37:58 -0700

    Initial addition of LCM calculator

---
+++ src/memmg.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int get_block_size(int bytes_in_struct, int bytes_from_os) {
+    //Finds lowest common multiple of `bytes_in_struct` and `bytes_from_os`
+    //
+    // Parameters
+    // ----------
+    //  bytes_in_stuct : int
+    //      size of struct
+    //  bytes_from_os : int
+    //      number of bytes allocated by OS at one time
+    //
+    // Returns
+    // -------
+    //  lcm : int
+    //      lowest common multiple of `bytes_in_struct` and `bytes_from_os`
+
+
+    int bytes_in_struct, bytes_from_os, lcm;
+    cout << "Enter two numbers: ";
+    cin >> bytes_in_struct >> bytes_from_os;
+
+    lcm = (bytes_in_struct > bytes_from_os) ? bytes_in_struct : bytes_from_os;
+
+    do {
+        if (lcm%bytes_in_struct == 0 && lcm%bytes_from_os == 0) {
+            cout << "LCM = " << lcm << endl;
+            break;
+        }
+        else
+            ++lcm;
+    }
+    while (true);
+
+    return 0;
+}
